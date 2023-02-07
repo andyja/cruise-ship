d4e4c800ed566f75e18c4dffd28534de677180ec
Andy Jackson <andypauljackson@gmail.com>
2023-02-07 09:55:27 +0000

    gui project setup

--- src/Port.js
+++ src/Port.js
@@ -1,5 +1,21 @@
-function Port(name) {
-    this.name = name;
-}
+class Port{
+    constructor (name){
+        this.name = name;
+        this.ships = [];
+    };
+
+    addShip(ship){
+        this.ships.push(ship);
+    };
+    
+removeShip(ship){
+
+    const indexOfShipToRemove = this.ships.indexOf(ship);
+    if (indexOfShipToRemove > -1){
+        this.ships.splice(indexOfShipToRemove, 1);
+    };
+    return this.ships;
+};
+};
 
 module.exports = Port;
--- src/Ship.js
+++ src/Ship.js
@@ -1,14 +1,31 @@
-class Ship(port, name, weight) {
-    this.port = port;
-    this.name = name;
-    this.weight = weight;
-    this.setSail = function () {
-        if (this.setSail) {
-            this.currentPort = false;
-        }
-    }
-    this.dock = function (port) {
-        this.currentPort = port;
-    }
- };
+class Ship { 
+    constructor(itinerary){
+    this.itinerary = itinerary;
+    this.currentPort = itinerary.ports[0];
+    this.previousPort = null;
+    this.currentPort.addShip(this);
+    };
+    
+    setSail(){
+        const itinerary = this.itinerary;
+        const currentPorkIndex = itinerary.ports.indexOf(this.currentPort);
+
+        if (currentPortIndex === (itinerary.porrts.length)-1) {
+    throw new Error ("End of journery")
+}
+
+this.previousPort = this.currentPort;
+this.currentPort = null;
+this.previousPort.removeShip(this);
+};
+
+dock() {
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+
+    this.currentPort = itinerary.ports[previousPortIndex + 1]
+    this.currentPort.addShip(this);
+};
+};
+
 module.exports = Ship;
