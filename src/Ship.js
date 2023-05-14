39be31aa8c22d8393d7dea48de2e85f2503ce1a5
Andy Jackson <andypauljackson@gmail.com>
2023-05-14 10:32:59 +0100

    updated ship object

--- src/Ship.js
+++ src/Ship.js
@@ -1,40 +1,35 @@
 (function exportShip() {
-    class Ship { 
-      constructor(itinerary) {
-        this.itinerary = itinerary;
-        this.currentPortIndex = 0; // initialize currentPortIndex to zero
-        this.currentPort = itinerary.ports[0];
-        this.previousPort = null;
-        this.currentPort.addShip(this);
-      }
+  class Ship { 
+      constructor(itinerary){
+      this.itinerary = itinerary;
+      this.currentPort = itinerary.ports[0];
+      this.previousPort = null;
+      this.currentPort.addShip(this);
+      };
       
-      setSail() {
-        if (this.itinerary.ports.length === 0) {
-          throw new Error("There are no ports in the itinerary.");
-        }
-        
-        if (this.currentPortIndex === this.itinerary.ports.length - 1) {
-          throw new Error("This is the last port in the itinerary.");
-        }
-        
-        this.currentPort.removeShip(this);
-        this.currentPortIndex++;
-        this.currentPort = this.itinerary.ports[this.currentPortIndex];
-        this.currentPort.addShip(this);
-      }
-        
-      dock() {
-        const itinerary = this.itinerary;
-        const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
-        this.currentPort = itinerary.ports[previousPortIndex + 1];
-        this.currentPort.addShip(this);
-      }
+      setSail() {const itinerary = this.itinerary;
+        const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+    if (currentPortIndex === (itinerary.ports.length)-1) {
+        throw new Error ('End of itinerary reached');
     }
+    this.previousPort = this.currentPort;
+    this.currentPort = null;
+    this.previousPort.removeShip(this);
+    };
+        
   
-    if(typeof module !== 'undefined' && module.exports){
+  dock() {
+      const itinerary = this.itinerary;
+      const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+      this.currentPort = itinerary.ports[previousPortIndex + 1];
+      this.currentPort.addShip(this);
+  };
+  };
+  
+  if(typeof module !== 'undefined' && module.exports){
       module.exports = Ship;
-    } else {
-      window.Ship = Ship;
-    }
+      } else {
+          window.Ship = Ship;
+      }
   })();
-  
